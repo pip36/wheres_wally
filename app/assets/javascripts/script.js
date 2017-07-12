26caceb9b1468bdac4e6664bb5f0f289fc105794
pip36 <phil_23@live.co.uk>
2017-07-12 15:19:52 +0100

    remove http from post

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -46,7 +46,7 @@ if ((url.slice(-1).search(/\d/)) == 0){
                 //score model name:"phil" score:"float"
                 //resources only:[:new]
                 //controller accept json request and add the data to the database
-                $.post("https://floating-everglades-97160.herokuapp.com/scores", {name: playerName, time: finalTime, id: imageID}, null, "json");
+                $.post("floating-everglades-97160.herokuapp.com/scores", {name: playerName, time: finalTime, id: imageID}, null, "json");
               }
             }
       }
